b35feed04c1e77658d0321ab26bf5703a611e35d
Jehan Fernando <02jehanfernando@gmail.com>
2026-09-24 11:08:18 +0530

    feat: VStack prac

--- Prac1/ContentView.swift
+++ Prac1/ContentView.swift
@@ -9,16 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            Rectangle()
+               .foregroundColor(.red)
+               .frame(width: 200, height: 100)
+           
+            Rectangle()
+               .foregroundColor(.blue)
+               .frame(width: 150, height: 80)
+           
+            Rectangle()
+               .foregroundColor(.green)
+               .frame(width: 250, height: 120)
+                
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
 }
+
+
+
