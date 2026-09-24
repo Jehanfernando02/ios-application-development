f3438991558a7340e1520e6354bc5d7d5c74dd79
Jehan Fernando <02jehanfernando@gmail.com>
2026-09-24 09:53:35 +0530

    Initial Commit

---
+++ Prac1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Prac1
+//
+//  Created by Jehan Fernando on 2026-09-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Prac1/Prac1App.swift
@@ -0,0 +1,17 @@
+//
+//  Prac1App.swift
+//  Prac1
+//
+//  Created by Jehan Fernando on 2026-09-24.
+//
+
+import SwiftUI
+
+@main
+struct Prac1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
